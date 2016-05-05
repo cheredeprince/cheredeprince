c5dacfba01fea79cf5a4e20da896afb341498372
emixam150 <emixam150@gmail.com>
2016-05-05 17:11:39 +0200

    test indentation

--- api/models/MathElt.js
+++ api/models/MathElt.js
@@ -2,14 +2,14 @@
  * MathElt.js
  *
  * @description :: TODO: Ce fichier décrit le fonctionnement des éléments mathématiques
- * @docs        :: 
+ * @docs        ::
  */
 
 var Lodash = require('lodash'),
     Twitter= require('twitter-text');
 
 var types = {
-    
+
     def : "définition",
     lem : "lemme",
     prop: "propriété",
@@ -27,85 +27,85 @@ module.exports = {
 	    return mathName;
 	},
 	uniqueName: function(value) {
-	    return uniqueName;	    
+	    return uniqueName;
 	}
     },
 
-    shema: true, 
+    shema: true,
 
     attributes: {
-	
+
 	name: {
 	    type: 'string',
 	    mathName: true,
 	    required: true,
 	    uniqueName: true
 	},
-	
+
 	oldNames:{
 	    type: 'array'
 	},
-	
+
 	title: {
 	    type: 'string',
 	    required: true
 	},
-	
+
 	type:{
 	    type: 'string',
 	    'enum': Object.keys(types),
 	    required: true
 	},
-	
+
 	published:{
 	    type: 'boolean',
 	    defaultsTo: true
 	},
-	
+
 	publishedAt:{
-	    type: 'date'  
+	    type: 'date'
 	},
-	
+
 	author: {
 	    model: 'User'
 	},
-	
+
 	content:{
 	    type: 'text',
 	    required :true
 	},
-	
+
 	contentHTML:{
 	    type:'text'
 	},
-	
+
 	tags:{
 	    collection: 'MathTag',
 	    via: 'elements'
 	},
-	
+
 	tagsName:{
 	    type: 'array'
 	},
-	
+
 	ems:{
 	    type: 'array'
 	},
-	
+
 	strongs: {
 	    type: 'array'
 	},
-	
+
 	parents:{
 	    collection: 'MathElt',
 	    via: 'children'
 	},
-	
+
 	parentsName:{
 	    type: 'array',
 	    defaultsTo: []
 	},
-	
+
 	children:{
 	    collection: 'MathElt',
 	    via: 'parents'
@@ -115,41 +115,41 @@ module.exports = {
 	    type: 'array',
 	    defaultsTo: []
 	},
-	
+
 	keywords:{
 	    collection: 'MathKeyword',
 	    via: 'elements'
 	},
-	
+
 	scorePerKeyword:{
 	    type:'json'
 	},
-	
+
 	exprPerKeyword:{
 	    type: 'json'
 	},
-	
+
 	keywordsName:{
 	    type: 'array'
 	},
-	
+
 	version_previous: {
 	    type: 'array',
 	    defaultsTo: []
 	},
-	
+
 	version_number: {
 	    type: 'integer',
 	    defaultsTo: 0
 	},
-	
+
 	toOBJ: function(){
 	    //retourne l'objet en élément à envoyer
 
 	    var elt = this,
 		obj = this.toObject(),
 		return_obj = {};
-	    
+
 	    return_obj.id = obj.id
 	    return_obj.name = obj.name
 	    return_obj.title = obj.title
@@ -157,7 +157,7 @@ module.exports = {
 	    return_obj.content = obj.contentHTML
 	    return_obj.tags = obj.tagsName
 	    return_obj.createdAt = obj.createdAt
-	    
+
 	    //mise en forme des parties de texte
 	    return_obj.typeDisplay = types[elt.type]
 
@@ -174,7 +174,7 @@ module.exports = {
 			type : obj.parents[key].type
 		    })
 		}
-	    
+
 	    if(obj.children && obj.children.length){
 		for(key = 0; key< obj.children.length;key++){
 		    return_obj.children.push({
@@ -184,7 +184,7 @@ module.exports = {
 		    })
 		}
 	    }
-	    
+
 	    return(return_obj)
 	},
 
@@ -201,21 +201,21 @@ module.exports = {
 	    return_obj.type = obj.type;
 	    return_obj.content = obj.content;
 	    return_obj.version_number = (typeof obj.version_number == 'number')? obj.version_number: 0;
-	    
+
 	    // les infos ci-dessous ne sont pas necessaire pour la restauration
 	    return_obj.updatedAt = obj.updatedAt;
 	    return_obj.author = obj.author;
-	    
+
 	    return(return_obj)
 	},
-	
+
 	toMiniOBJ: function(){
 	    //retroune une description de l'élément
-	    
+
 	    var elt = this,
 		obj = this.toObject(),
 		return_obj={};
-	    
+
 	    return_obj = {
 		name : obj.name,
 		title: obj.title,
@@ -226,9 +226,9 @@ module.exports = {
     },
 
     /************************************* GENERAL MATH METHOD **************************************************/
-    
+
     beforeValidate: function(values,next){
-	
+
 	// on vérifie que le nom est bien un URI
 	mathName = values.name == Lodash.snakeCase(values.name) && Twitter.isValidUsername('@'+values.name);
 
@@ -238,9 +238,9 @@ module.exports = {
 	    next();
 	});
     },
-    
+
     createElt: function(values, next){
-	
+
 	beforeCreateOrUpdate(values,function(){
 
 	    // on met en place les collections
@@ -252,7 +252,7 @@ module.exports = {
 
 	    var atEnd = function(){
 		// fonction appelée à la fin de chaque partie
-		
+
 		cpt++;
 		if(cpt == length){
 
@@ -292,10 +292,10 @@ module.exports = {
 
 		//on récupère les keywords de tous les parents
 		parentsKeywordsName = _.flatten(_.pluck(parents,'keywordsName'));
-		
+
 		atEnd();
 	    });
-	    
+
 	    //Partie 2
 	    /*mise en place des tags*/
 	    MathTag.findOrCreateListNames(values.tagsName,function(err,tags){
@@ -303,13 +303,12 @@ module.exports = {
 
 		//on met à jour les tags
 		values.tags = _.pluck(tags,'id');
-		
+
 		atEnd()
 	    });
-	    
 	});
     },
-    
+
     updateElt: function(id,values, next){
 	//mise à jour d'un élément
 
@@ -320,41 +319,40 @@ module.exports = {
 		.populate('tags')
 		.populate('parents')
 		.exec(function(err,elt){
-		    
+
 		    if(err) return next(err)
 		    if(!elt) return next();
-		    
-		    
+
 		    var cpt = 0,
 			length = 2,
 			parentsKeywordsName,
 			oldName = elt.name,
 			removedParents,
 			addedParents;
-		    
+
 		    var atEnd = function(){
 			// fonction appélée à la fin de chaque partie
-			
+
 			cpt++;
 			if(cpt == length){
 			    setKeywords(values,parentsKeywordsName,function(err){
-				
+
 				if(err) return next(err);
-				
+
 				values.id = id;
 
-				console.time('mongo');	
+				console.time('mongo');
 
 				MathElt.update(id,values,function(err){
-p
-				    console.timeEnd('mongo');	
+
+				    console.timeEnd('mongo');
 				    if(err) return next(err);
 
 				    // on prévient qu'il y a une maj
 				    MathElt.publishUpdate(id,{name: values.name});
-				    
+
 				    afterUpdateElt(values,oldName,removedParents,addedParents,next);
-				    
+
 				});
 			    });
 			}
@@ -362,14 +360,14 @@ p
 
 		    /* mise à jour des versions */
 		    values.version_previous = (elt.version_previous)? elt.version_previous:[];
-		    
+
 		    // on sauve l'ancienne version
 		    values.version_previous.push(elt.toVersion());
-		    
+
 		    //on incrémente de numéro de version
 		    values.version_number = (typeof elt.version_number == 'number')? elt.version_number +1:0;
 
-		    
+
 		    /*ajout d'un ancien nom*/
 		    if(values.name != elt.name)
 			values.oldNames = (elt.oldNames)?elt.oldNames.concat([elt.name]): [elt.name];
@@ -378,7 +376,7 @@ p
 		    /*mise en place des parents et ancetres */
 		    MathElt.find({name: values.parentsName},function(err,parents_updated){
 			if(err) return next(err)
-			
+
 			// construit les nouveaux ancetres = ancetres des parents + parents
 			var pureAncestors = _.flatten(_.pluck(parents_updated,'ancestors')),
 			    parentsName  = _.pluck(parents_updated,'name'),
@@ -402,7 +400,7 @@ p
 			    parentsKeywordsName = _.flatten(_.pluck(parents_updated,"keywordsName"));
 			    addedParents = _.difference(parentsName,elt.parentsName);
 			    removedParents = _.difference(elt.parentsName,parentsName);
-			    
+
 			    atEnd();
 			}else{
 			    //on a des prolblèmes de générations
@@ -432,7 +430,7 @@ p
 		});
 	});
     },
-    
+
     restore: function(id,version_number, author_id,next){
 	// cette fonction restaure la version numéro version_number de l'élément id, si les deux existent
 	// on récupére l'elt à restaurer
@@ -446,7 +444,7 @@ p
 		version,
 		values = {},
 		author = (author_id)? author_id : '';
-	    
+
 	    //s'il existe une telle version
 	    if(index != -1){
 		//on recopie les infos utiles à la maj
@@ -469,14 +467,14 @@ p
 		next();
 	})
     },
-    
+
     completeName : function(beginName,next){
 
 	MathElt.find({name:RegExp("^"+Lodash.snakeCase(beginName)+"[a-zA-Z_]*$","g")}).populate('children').exec(function(err,elts){
 	    if(err) return next(err);
-	    
+
 	    var completedNames = _.pluck(ModelAssets.sortWithUpdate(elts,'.children.length',12,10),"name")
-	    
+
 	    next(err,completedNames)
 	})
     }
@@ -488,23 +486,23 @@ var beforeCreateOrUpdate, afterCreateElt, afterUpdateElt, updateName, setKeyword
 
 beforeCreateOrUpdate = function(values, next){
     // créer des valeurs à partir du contenu utilisateur
-    
+
     values.name = (values.name)? values.name:'';
     values.content = (values.content)? values.content:'';
-    
+
     ConvertString.parseMathText(values.content,function(obj){
-	
+
 	values.parentsName = obj.parents;
 	//on évite le cas de la boucle simple
-	
+
 	if(values.parentsName.indexOf(values.name) !== -1)
 	    delete values.parentsName[values.parentsName.indexOf(values.name)]
-	
+
 	values.tagsName = obj.tags;
 	values.ems = obj.ems;
 	values.strongs = obj.strongs;
 	values.contentHTML = obj.html;
-	
+
 	next();
     })
 };
@@ -515,7 +513,7 @@ afterCreateElt = function(values, next){
     //On met à jour l'élément au sein du graphe
     MathGraph.newElement(values.name,values.title,values.type,values.parentsNotAncestors,function(err){
 	if(err) throw err;
-    });	
+    });
 };
 
 afterUpdateElt= function(values,oldName,removedParents,addedParents,next){
@@ -525,7 +523,7 @@ afterUpdateElt= function(values,oldName,removedParents,addedParents,next){
     MathGraph.updateElement(oldName,values.name,values.title,values.type,values.parentsNotAncestors,function(err){
 	if(err) throw err;
 	if(oldName == values.name) return;
-	
+
 	//puis on met à jour le nom dans les éléments
 	updateName(oldName,values.name,function(err){
 	    if(err) throw err;
@@ -567,19 +565,19 @@ updateName = function(oldName,name,next){
  * Attribue les valeurs de kw kwName,scoreperkw et exprPerkw
  */
 setKeywords = function(values,parentsKeywordsName,next){
-    
+
     //initialisation
     values.keywords       = [];
     values.keywordsName   = [];
     values.exprPerKeyword = {};
     values.scorePerKeyword= {};
     var subSetter= function(sentences,scoreTotal,next){
-	
+
 	var key;
-	
+
 	MathKeyword.extractAndRecordMulti(sentences,function(err,kws,exprPerKw){
 	    if(err) return next(err);
-	    
+
 	    values.keywords       = _.union(values.keywords,_.pluck(kws,"id"));
 	    values.keywordsName   = _.union(values.keywordsName,_.pluck(kws,"name"));
 
@@ -587,13 +585,13 @@ setKeywords = function(values,parentsKeywordsName,next){
 	    var scorePerkw =(kws.length !=0)? parseInt(scoreTotal/(2*kws.length)): 0;
 
 	    for(key = 0; key<kws.length;key++){
-		
+
 		//si on a pas encore ajouté ce mot clé
 		if(!values.exprPerKeyword[kws[key].name])
 		    values.exprPerKeyword[kws[key].name] = exprPerKw[kws[key].name];
 		else
 		    values.exprPerKeyword[kws[key].name] = _.union(values.exprPerKeyword[kws[key].name],exprPerKw[kws[key].name]);
-		
+
 		//suivant que le mot est composé ou non
 		if(values.scorePerKeyword[kws[key].name])
 		    values.scorePerKeyword[kws[key].name] += (kws[key].name.indexOf('_') == -1)? scorePerkw: 2*scorePerkw;
@@ -608,11 +606,11 @@ setKeywords = function(values,parentsKeywordsName,next){
 	ConvertString.dropTags(content,function(content,tags){
 	    //		MathKeyword.find({},function(err,keywords){
 	    //		    if(err) return next(err);
-	    
+
 	    var heritedExprs = _.compact(_.values(ConvertString.searchKeywords(content,parentsKeywordsName)));
 	    //	contentExprs = _.compact(_.values(ConvertString.searchKeywords(content,_.pluck(keywords,"name"))));
-	    
-	    //mettre à jour les keys issue des parents		  
+
+	    //mettre à jour les keys issue des parents
 	    subSetter(heritedExprs,300,function(err){
 		if(err) return next(err);
 		//puis les tags
